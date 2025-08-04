a50a204a16e4c269e78525da5ae3449b9cb0f8e3
AbdulTechX <abdulazeezbuhari27@gmail.com>
2025-08-04 22:45:51 +0100

    Basic Routing in Next.js

---
+++ alx-project-2/components/layout/Header.tsx
@@ -0,0 +1,13 @@
+const header: React.FC = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="/home"></a>Home</li>
+                    <li><a href="about">About</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default header;
---
+++ alx-project-2/pages/about.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Aboutpage: React.FC = () => {
+    return(
+        <div>
+            <h1> Welcome to the Aboutpage </h1>
+        </div>
+    )
+}
+export default Aboutpage;
